dc153b50fe0d64e70ed7374b0bf6e0b8f02c621a
Andy Truong <thehongtt@gmail.com>
2016-08-26 08:05:05 +0700

    Misc on start.php

--- start.php
+++ start.php
@@ -5,4 +5,5 @@ $pwd = __DIR__;
 passthru("mkdir $pwd/.data/nginx/conf.d");
 passthru("touch $pwd/.data/nginx/conf.d/default.conf");
 passthru("cp $pwd/.data/nginx/app.conf $pwd/.data/nginx/conf.d/app.conf");
-passthru("docker-compose up --force-recreate");
+passthru("docker-compose up");
+# passthru("docker-compose restart &");
