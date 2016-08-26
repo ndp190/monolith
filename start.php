867b5640bdebca9528ea47ae023a08df5cf903ce
Andy Truong <thehongtt@gmail.com>
2016-08-26 10:06:40 +0700

    misc

--- start.php
+++ start.php
@@ -1,7 +1,6 @@
 <?php
 
 $pwd = __DIR__;
-
 passthru("mkdir -p $pwd/.data/nginx/conf.d");
 passthru("touch $pwd/.data/nginx/conf.d/default.conf");
 passthru("cp $pwd/.data/nginx/app.conf $pwd/.data/nginx/conf.d/app.conf");
