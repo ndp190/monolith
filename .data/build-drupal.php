35f823acfe5d84646393d462e0d0917700b49cd8
Andy Truong <thehongtt@gmail.com>
2016-08-29 08:24:56 +0700

    Build drupal: composer install

--- .data/build-drupal.php
+++ .data/build-drupal.php
@@ -3,16 +3,6 @@
 namespace at\labs;
 
 return function ($pwd, $home) {
-  if (!file_exists("$pwd/.data/cli/drush")) {
-    passthru("mkdir $pwd/.data/cli");
-    passthru("wget https://s3.amazonaws.com/files.drush.org/drush.phar -O $pwd/.data/cli/drush");
-    passthru("chmod +x $pwd/.data/cli/drush");
-  }
-
-  if (!is_dir("$pwd/.data/drupal")) {
-    passthru("mkdir $pwd/.data/drupal");
-  }
-
   $docker = "docker run --rm"; # -it
   $docker .= " -v $pwd/drupal/:/app/";
   $docker .= " -v $pwd/.data/cli/:/cli/";
@@ -22,15 +12,18 @@ return function ($pwd, $home) {
   $docker .= " -w=/drupal/ go1com/php:php7";
   $php = "$docker php";
   $drush = "$php /cli/drush";
+  $composer = "$php /cli/composer";
 
-  print_r([$drush]);
-  # exit;
+  if (!file_exists("$pwd/.data/cli/drush")) {
+    passthru("mkdir $pwd/.data/cli");
+    passthru("wget https://s3.amazonaws.com/files.drush.org/drush.phar -O $pwd/.data/cli/drush");
+    passthru("chmod +x $pwd/.data/cli/drush");
+  }
 
+  // Create Drupal directory
+  !is_dir("$pwd/.data/drupal") && passthru("mkdir $pwd/.data/drupal");
+
+  // Build Drupal
   passthru("$drush make /app/gc/build/build.make -y -vvv");
-
-  # rm -rf builds/drupal
-  # drush make apps/gc/build/build.make builds/drupal
-  # cd builds/drupal/profiles && ln -s ../../../apps/gc gocatalyze && cd -
-  # cd apps/gc/vendor && composer install --ignore-platform-reqs && cd -
-  # mv sites/all/vendor builds/drupal/sites/all/vendor
+  passthru("cd /drupal/profiles/gocatalyze/vendor/ && $composer --ignore-platform-reqs install");
 };
