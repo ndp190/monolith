cd8921ed8737b45c5c34b93c84f46da77c27b386
Andy Truong <thehongtt@gmail.com>
2016-11-07 21:01:01 +0700

    Handle exception in quiz.

--- .data/build-git-pull.php
+++ .data/build-git-pull.php
@@ -3,15 +3,22 @@
 namespace at\labs;
 
 return function ($pwd, $pull, $projects) {
+    $branches = [
+        'quiz'    => '1.x',
+        'default' => 'master',
+    ];
+
     foreach ($projects as $lang => $services) {
         foreach ($services as $name => $path) {
+            $branch = isset($branches[$name]) ? $branches[$name] : $branches['default'];
+
             if (!is_dir("$pwd/$lang/$name")) {
-                print_r("git clone -q --branch=master $path $pwd/$lang/$name\n");
-                passthru("git clone -q --branch=master $path $pwd/$lang/$name");
+                print_r("git clone -q --branch={$branch} $path $pwd/$lang/$name\n");
+                passthru("git clone -q --branch={$branch} $path $pwd/$lang/$name");
             }
             elseif ($pull) {
-                print_r("git pull -q origin master\n");
-                passthru("cd $pwd/$lang/$name && git pull -q --single-branch --branch=master origin master && cd $pwd");
+                print_r("git pull -q origin {$branch}\n");
+                passthru("cd $pwd/$lang/$name && git pull -q --single-branch --branch={$branch} origin master && cd $pwd");
             }
         }
     }
