21014754412185f29d31c9d2b8d6c68eb5d55faf
Andy Truong <thehongtt@gmail.com>
2016-12-07 09:33:53 +0700

    Include SSO.

--- scripts/build.php
+++ scripts/build.php
@@ -43,6 +43,7 @@ $projects = [
         'console'      => 'git@code.go1.com.au:go1/console.git',
         'boss'         => 'git@code.go1.com.au:go1/worker_manager.git',
         's3'           => 'git@code.go1.com.au:microservices/s3.git',
+        'sso'          => 'git@code.go1.com.au:microservices/sso.git',
     ],
     'php/libraries'  => [
         'app'            => 'git@github.com:go1com/app.git',
