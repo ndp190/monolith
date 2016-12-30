88c6116d5eb329c1f5cfb883aaa2b87a0a5a2e84
Andy Truong <thehongtt@gmail.com>
2016-12-30 13:31:43 +1000

    Generate: Replace XXXXX - upper case.

--- scripts/git/generate.php
+++ scripts/git/generate.php
@@ -5,17 +5,18 @@ require_once __DIR__ . '/../../php/vendor/go1.autoload.php';
 return call_user_func(function () {
     // Change this when you run the script
     $name = 'foo';
-    $camel = ucfirst($name);
     $targetRoot = __DIR__ . '/foo';
 
     // Should not change code below
+    $upper = strtoupper($name);
+    $camel = ucfirst($name);
     $templateRoot = __DIR__ . '/generate';
-    $copy = function ($file) use ($templateRoot, $targetRoot, $name, $camel) {
-        $path = str_replace([$templateRoot, 'Xxxxx', 'xxxxx'], ['', $camel, $name], $file);
+    $copy = function ($file) use ($templateRoot, $targetRoot, $name, $upper, $camel) {
+        $path = str_replace([$templateRoot, 'XXXXX', 'Xxxxx', 'xxxxx'], ['', $upper, $camel, $name], $file);
         $path = trim($path, '/');
         $path = $targetRoot . '/' . $path;
         $content = file_get_contents($file);
-        $content = str_replace([$templateRoot, 'Xxxxx', 'xxxxx'], ['', $camel, $name], $content);
+        $content = str_replace([$templateRoot, 'XXXXX', 'Xxxxx', 'xxxxx'], ['', $upper, $camel, $name], $content);
         $dir = dirname($path);
         !is_dir($dir) && passthru("mkdir -p $dir");
         file_put_contents($path, $content);
