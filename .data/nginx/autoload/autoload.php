6e0a43c6c58def2a6bdde516b4cd24d987ad5c49
Andy Truong <thehongtt@gmail.com>
2017-01-04 16:06:03 +1000

    Define __db_connection_options().

--- .data/nginx/autoload/autoload.php
+++ .data/nginx/autoload/autoload.php
@@ -2,6 +2,19 @@
 
 use Composer\Autoload\ClassLoader;
 
+function db_connection_options()
+{
+    return [
+        'driver'        => 'pdo_mysql',
+        'dbname'        => "go1_dev",
+        'host'          => 'mysql',
+        'user'          => 'root',
+        'password'      => 'root',
+        'port'          => '3306',
+        'driverOptions' => [1002 => 'SET NAMES utf8'],
+    ];
+}
+
 if (isset($_SERVER['REQUEST_URI'])) {
     if (0 === strpos($_SERVER['REQUEST_URI'], '/GO1/')) {
         $_SERVER['REQUEST_URI'] = preg_replace('`^/GO1/[a-z0-9]+/(.*)$`', '/$1', $_SERVER['REQUEST_URI']);
