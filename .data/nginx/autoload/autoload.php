05482fa26b23b5de120dff92392acb07e161b9b7
Andy Truong <thehongtt@gmail.com>
2017-01-04 15:03:18 +1000

    Fix some warning when we run phpunit test inside docker.

--- .data/nginx/autoload/autoload.php
+++ .data/nginx/autoload/autoload.php
@@ -2,16 +2,18 @@
 
 use Composer\Autoload\ClassLoader;
 
-if (0 === strpos($_SERVER['REQUEST_URI'], '/GO1/')) {
-    $_SERVER['REQUEST_URI'] = preg_replace('`^/GO1/[a-z0-9]+/(.*)$`', '/$1', $_SERVER['REQUEST_URI']);
-}
+if (isset($_SERVER['REQUEST_URI'])) {
+    if (0 === strpos($_SERVER['REQUEST_URI'], '/GO1/')) {
+        $_SERVER['REQUEST_URI'] = preg_replace('`^/GO1/[a-z0-9]+/(.*)$`', '/$1', $_SERVER['REQUEST_URI']);
+    }
 
-if (0 === strpos($_SERVER['REQUEST_URI'], '/v3/')) {
-    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 3);
-}
+    if (0 === strpos($_SERVER['REQUEST_URI'], '/v3/')) {
+        $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 3);
+    }
 
-if (0 === strpos($_SERVER['REQUEST_URI'], '//')) {
-    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 1);
+    if (0 === strpos($_SERVER['REQUEST_URI'], '//')) {
+        $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 1);
+    }
 }
 
 /** @var ClassLoader $loader */
