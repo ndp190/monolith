57fcb0f0e0643d550b95c3cb50c239af5bf0ed0b
Andy Truong <thehongtt@gmail.com>
2017-01-13 11:27:25 +1000

    Code generating: Provide 2 default routes
    - GET /cron
    - POST /consume with access checker.

---
+++ scripts/git/generate/controller/CronController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace go1\xxxxx\controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class CronController
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function get(Request $req)
+    {
+        $this->logger->debug('Cron start.');
+
+        return new JsonResponse(null, 204);
+    }
+}
--- scripts/git/generate/domain/XxxxxServiceProvider.php
+++ scripts/git/generate/domain/XxxxxServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace go1\xxxxx\domain;
 
+use go1\xxxxx\controller\ConsumeController;
+use go1\xxxxx\controller\CronController;
 use go1\xxxxx\controller\InstallController;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
@@ -15,10 +17,20 @@ class XxxxxServiceProvider implements ServiceProviderInterface, BootableProvider
         $c['ctrl.install'] = function (Container $c) {
             return new InstallController($c['dbs']['default']);
         };
+
+        $c['ctrl.consumer'] = function (Container $c) {
+            return new ConsumeController($c['logger'], $c['access_checker']);
+        };
+
+        $c['ctrl.cron'] = function (Container $c) {
+            return new CronController($c['logger']);
+        };
     }
 
     public function boot(Application $app)
     {
         $app->get('/install', 'ctrl.install:get');
+        $app->post('/consume', 'ctrl.consumer:post');
+        $app->get('/cron', 'ctrl.cron:get');
     }
 }
