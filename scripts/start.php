afb1f2d4f0a58300ac8ba5e6b1fcbbdc8ed71a14
Andy Truong <thehongtt@gmail.com>
2017-03-09 16:14:45 +0700

    Make sure we have database for all services.

--- scripts/start.php
+++ scripts/start.php
@@ -6,3 +6,11 @@ passthru("touch $pwd/.data/nginx/sites-available/default.conf");
 passthru("cp $pwd/.data/nginx/app.conf $pwd/.data/nginx/sites-available/default.conf");
 passthru("docker-compose up --force-recreate"); # docker-compose restart &
 echo "Ctrl+c to stop services.\n";
+
+sleep(30);
+
+// Make sure we have database for all services
+$projects = require __DIR__ . '/_projects.php';
+foreach ($projects['php'] as $name) {
+    file_get_contents("http://localhost/GO1/{$name}/install");
+}
