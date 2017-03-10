bd8fb023a3af14e7b9ea88788921acc788b1a0ad
Andy Truong <thehongtt@gmail.com>
2017-03-10 10:18:09 +0700

    Correct configuration for #ui - follow up.

--- .data/resources/docker/fix-apiom-ui.php
+++ .data/resources/docker/fix-apiom-ui.php
@@ -1,32 +1,23 @@
 <?php
 
 $config = file_get_contents('/apiomui/scripts/config.apiom.js');
-if (!strpos('#MONOLITH', $config)) {
+if (!strpos('localhost/GO1/', $config)) {
     $config = str_replace('api-dev.mygo1.com', 'localhost/GO1', $config);
-    $config = str_replace('https://localhost/GO1/v3/', 'localhost/v3/', $config);
+    $config = str_replace('https://localhost/GO1/v3/', 'http://localhost/GO1/', $config);
     file_put_contents('/apiomui/scripts/config.apiom.js', $config);
-}
-
-if (true) {
-    return null;
 }
 
 $html = file_get_contents('/apiomui/index.html');
 if (!strpos('#MONOLITH', $html)) {
     $local = "\n\n<!-- ====================================== #MONOLITH ==================================================== -->\n";
     $local .= '<script src="scripts/config.apiom.js"></script>' . "\n";
-    $local .= '<script src="scripts/app.js"></script>' . "\n";
-    $local .= '<script src="scripts/config.js"></script>' . "\n";
-    $local .= "<!-- ========================================================================================== -->\n\n";
 
     $matches = [];
     preg_match_all(
-        '`(<script src="//cdn.go1static.com/assets/[0-9\-]+/js/vendor.[0-9a-z]+.js"></script>)`i',
+        '`(<script src="//cdn.go1static.com/assets/[0-9\-]+/js/scripts.[0-9a-z]+.js"></script>)`i',
         $html,
         $matches
     );
 
-    $html = str_replace($matches[1][0], $matches[1][0] . $local, $html);
-
-    file_put_contents('/apiomui/index.html', $html);
+    file_put_contents('/apiomui/index.html', str_replace($matches[1][0], $matches[1][0] . $local, $html));
 }
