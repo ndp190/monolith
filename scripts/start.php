8a7a73c87779a535dc992691051f63d1e620262e
Andy Truong <thehongtt@gmail.com>
2017-03-10 15:03:02 +0700

    consumer container: Correct service URL pattern.

--- scripts/start.php
+++ scripts/start.php
@@ -7,7 +7,6 @@ passthru("cp $pwd/.data/nginx/app.conf $pwd/.data/nginx/sites-available/default.
 passthru("docker-compose start"); # docker-compose restart &
 
 # Make sure we have database for all services
-# sleep(3);
 $projects = require __DIR__ . '/_projects.php';
 foreach (array_keys($projects['php']) as $name) {
     echo "[install] GET http://localhost/GO1/{$name}/install\n";
