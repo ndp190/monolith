42538211a81b239ad5feb050c2f79152641bae33
Andy Truong <thehongtt@gmail.com>
2017-03-10 13:57:02 +0700

    “better” scripts/start.php

--- scripts/start.php
+++ scripts/start.php
@@ -4,14 +4,12 @@ $pwd = dirname(__DIR__);
 passthru("mkdir -p $pwd/.data/nginx/sites-available");
 passthru("touch $pwd/.data/nginx/sites-available/default.conf");
 passthru("cp $pwd/.data/nginx/app.conf $pwd/.data/nginx/sites-available/default.conf");
-passthru("docker-compose up --force-recreate"); # docker-compose restart &
-echo "Ctrl+c to stop services.\n";
-
-# Wait for all services to be started, this is not accurate.
-sleep(30);
+passthru("docker-compose start"); # docker-compose restart &
 
 # Make sure we have database for all services
+# sleep(3);
 $projects = require __DIR__ . '/_projects.php';
-foreach ($projects['php'] as $name) {
-    file_get_contents("http://localhost/GO1/{$name}/install");
+foreach (array_keys($projects['php']) as $name) {
+    echo "[install] GET http://localhost/GO1/{$name}/install\n";
+    @file_get_contents("http://localhost/GO1/{$name}/install");
 }
