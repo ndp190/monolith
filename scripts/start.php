1f761f43bc8a4f169ba0c09f059d01e59820ee01
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-03-13 13:51:44 +0700

    Split scripts

--- scripts/start.php
+++ scripts/start.php
@@ -2,26 +2,8 @@
 
 namespace go1\monolith;
 
-use mysqli;
-
 $pwd = dirname(__DIR__);
 passthru("mkdir -p $pwd/.data/nginx/sites-available");
 passthru("touch $pwd/.data/nginx/sites-available/default.conf");
 passthru("cp $pwd/.data/nginx/app.conf $pwd/.data/nginx/sites-available/default.conf");
-passthru("docker-compose up -d --force-recreate");
-
-# Make sure database 'go1_dev' is created.
-$db = new mysqli('127.0.0.1', 'root', 'root');
-if (!$db->select_db('go1_dev')) {
-    if (true !== $db->query('CREATE DATABASE go1_dev')) {
-        die("Failed to create 'go1_dev': {$db->error}\n");
-    }
-}
-$db->close();
-
-# Make sure we have database for all services
-$projects = require __DIR__ . '/_projects.php';
-foreach (array_keys($projects['php']) as $name) {
-    echo "[install] GET http://localhost/GO1/{$name}/install\n";
-    @file_get_contents("http://localhost/GO1/{$name}/install");
-}
+passthru("docker-compose up --force-recreate");
