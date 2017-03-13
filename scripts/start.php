2b49914940776e1123fb864df123d00370dccad2
Andy Truong <thehongtt@gmail.com>
2017-03-13 10:42:36 +0700

    misc

--- scripts/start.php
+++ scripts/start.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace go1\monolith;
+
 $pwd = dirname(__DIR__);
 passthru("mkdir -p $pwd/.data/nginx/sites-available");
 passthru("touch $pwd/.data/nginx/sites-available/default.conf");
@@ -7,20 +9,13 @@ passthru("cp $pwd/.data/nginx/app.conf $pwd/.data/nginx/sites-available/default.
 passthru("docker-compose up -d --force-recreate");
 
 # Make sure database 'go1_dev' is created.
-$conn = new mysqli('127.0.0.1', 'root', 'root');
-if ($conn->select_db('go1_dev')) {
-    echo "Database 'go1_dev' exists\n";
-}
-else {
-    echo "Database 'go1_dev' does not exists, creating...\n";
-    if ($conn->query('CREATE DATABASE go1_dev') === TRUE) {
-        echo "Database 'go1_dev' created successfully\n";
-    }
-    else {
-        echo "Error creating database 'go1_dev': " . $conn->error . "\n";
+$db = new mysqli('127.0.0.1', 'root', 'root');
+if (!$db->select_db('go1_dev')) {
+    if (true !== $db->query('CREATE DATABASE go1_dev')) {
+        die("Failed to create 'go1_dev': {$db->error}\n");
     }
 }
-$conn->close();
+$db->close();
 
 # Make sure we have database for all services
 $projects = require __DIR__ . '/_projects.php';
