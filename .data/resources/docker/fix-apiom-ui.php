08ebcbdb0493db4dd17d138cade113829f23dbc2
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-03-16 08:49:53 +0700

    Local domain for signup wizard

--- .data/resources/docker/fix-apiom-ui.php
+++ .data/resources/docker/fix-apiom-ui.php
@@ -4,6 +4,7 @@ $config = file_get_contents('/apiomui/scripts/config.apiom.js');
 if (!strpos('localhost/GO1/', $config)) {
     $config = str_replace('api-dev.mygo1.com', 'localhost/GO1', $config);
     $config = str_replace('https://localhost/GO1/v3/', 'http://localhost/GO1/', $config);
+    $config = str_replace('signup_wizard_domain:\'.mygo1.com\'', 'signup_wizard_domain:\'.go1.local\'', $config);
     file_put_contents('/apiomui/scripts/config.apiom.js', $config);
 }
 
