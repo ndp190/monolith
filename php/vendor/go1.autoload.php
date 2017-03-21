5a3c7799fabc6c826bf3eb67c63c9f70641406a2
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-03-21 19:06:20 +0700

    Fix autoload failed on some cases: go1/util, scraping

--- php/vendor/go1.autoload.php
+++ php/vendor/go1.autoload.php
@@ -9,15 +9,6 @@ if (is_file(__DIR__ . "/autoload.php")) {
     /** @var ClassLoader $loader */
     $loader = require_once __DIR__ . "/autoload.php";
 
-    $loader->addPsr4('go1\\clients\\', __DIR__ . '/../libraries/util/clients');
-
-    foreach ($loader->getPrefixesPsr4() as $ns => $paths) {
-        if (0 === strpos($ns, 'go1\\')) {
-            $path = explode('../../php/', $paths[0])[1];
-            $loader->addPsr4($ns, __DIR__ . '/../' . $path);
-        }
-    }
-
     if (!class_exists('PHPUnit_Framework_TestCase') && class_exists(TestCase::class)) {
         class PHPUnit_Framework_TestCase extends TestCase
         {
