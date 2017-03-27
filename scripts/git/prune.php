7834e449388c168aafe2f91d6202eed1527c7569
Andy Truong <thehongtt@gmail.com>
2017-03-27 09:24:26 +0700

    scripts/prune: Also prune #monolith.

--- scripts/git/prune.php
+++ scripts/git/prune.php
@@ -1,15 +1,21 @@
 <?php
 
+namespace go1\monolith\git;
+
 require_once __DIR__ . '/../../php/vendor/go1.autoload.php';
 
+function prune(string $target)
+{
+    $cmd = "cd $target && git fetch origin --prune";
+    echo "$ {$cmd}\n";
+    passthru($cmd);
+}
+
 return call_user_func(function () {
-    $dir = dirname(dirname(__DIR__));
+    prune($dir = dirname(dirname(__DIR__)));
+
     $projects = require __DIR__ . '/../_projects.php';
     foreach ($projects['php'] as $name => $path) {
-        $target = "{$dir}/php/{$name}";
-        $cmd = "cd $target && git fetch origin --prune";
-        echo "$ {$cmd}\n";
-
-        passthru($cmd);
+        prune("{$dir}/php/{$name}");
     }
 });
