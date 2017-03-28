adf1c8b0009de4a2b2da8a3bd4a4bea9c49671ef
Andy Truong <thehongtt@gmail.com>
2017-03-28 15:07:12 +0700

    code generate: Simplify queue options definition.

--- scripts/git/generate/config.default.php
+++ scripts/git/generate/config.default.php
@@ -16,11 +16,6 @@ return call_user_func(function () {
                 (getenv('CACHE_BACKEND') && 'memcached' === getenv('CACHE_BACKEND'))
                     ? ['backend' => 'memcached', 'host' => getenv('CACHE_HOST'), 'port' => getenv('CACHE_PORT')]
                     : ['backend' => 'filesystem', 'directory' => __DIR__ . '/cache'],
-            'queueOptions'  => [
-                'host' => getenv('QUEUE_HOST') ?: '172.31.11.129',
-                'port' => getenv('QUEUE_PORT') ?: '5672',
-                'user' => getenv('QUEUE_USER') ?: 'go1',
-                'pass' => getenv('QUEUE_PASSWORD') ?: 'go1',
-            ],
+            'queueOptions'  => Service::queueOptions(),
         ] + Service::urls(['rules', 'graphin'], $env, getenv('SERVICE_URL_PATTERN'));
 });
