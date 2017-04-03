be49b053d1015ffb321779c91801d91aa4707cce
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-04-03 10:27:15 +0700

    Create portal 'default.go1.local' by default

--- scripts/install.php
+++ scripts/install.php
@@ -11,6 +11,18 @@ if (!$db->select_db('go1_dev')) {
         die("Failed to create 'go1_dev': {$db->error}\n");
     }
 }
+# Make sure default portal 'default.go1.local' is created.
+if ($db->select_db('go1_dev')) {
+    if (empty($db->query("SELECT id FROM gc_instance WHERE title = 'default.go1.local'"))) {
+        $data = '{"author":"admin@default.go1.local","configuration":{"is_virtual":1,"user_invite":1,"send_welcome_email":1},"features":{"marketplace":true,"user_invite":true,"auth0":false},"user_plan":{"license":10,"price":3620,"product":"marketplace"}}';
+        $now = time();
+        $sql = "INSERT INTO gc_instance (title, status, is_primary, version, data, timestamp, created)
+            VALUES ('default.go1.local', 1, 1, 'v3.0.0', '{$data}', {$now}, {$now})";
+        if (true !== $db->query($sql)) {
+            die("Failed to insert 'default.go1.local': {$db->error}\n");
+        }
+    }
+}
 $db->close();
 
 # Make sure we have database for all services
