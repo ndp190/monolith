e02372ed1fcce15c67bb5aa4e49255ea3d18bbf7
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-04-17 17:27:44 +0700

    Fix syntax

--- scripts/build-web.php
+++ scripts/build-web.php
@@ -4,7 +4,7 @@ namespace go1\monolith;
 
 $pwd = dirname(__DIR__);
 
- -passthru("cd $pwd/web/ui && npm install");
- -passthru("cd $pwd/web/ui && bower install");
- -passthru("cd $pwd/web/ui && grunt set-env:monolith");
- -passthru("cd $pwd/web/ui && grunt build");
+passthru("cd $pwd/web/ui && npm install");
+passthru("cd $pwd/web/ui && bower install");
+passthru("cd $pwd/web/ui && grunt set-env:monolith");
+passthru("cd $pwd/web/ui && grunt build");
