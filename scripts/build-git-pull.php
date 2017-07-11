50acf4cea5386e57233a8ca35df17de429437822
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-07-11 09:20:29 +0700

    Update branch for quiz

--- scripts/build-git-pull.php
+++ scripts/build-git-pull.php
@@ -3,14 +3,10 @@
 namespace at\labs;
 
 return function ($pwd, $projects) {
-    $branches = [
-        'quiz'    => '1.x',
-        'default' => 'master',
-    ];
+    $branch = 'master';
 
     foreach ($projects as $lang => $services) {
         foreach ($services as $name => $path) {
-            $branch = isset($branches[$name]) ? $branches[$name] : $branches['default'];
             $target = ('golang' === $lang) ? "$pwd/$lang/src/vendor/go1/$name" : "$pwd/$lang/$name";
 
             if (!is_dir($target)) {
