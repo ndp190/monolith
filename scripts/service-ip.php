c27f1576d3ea667810e8a7d3f57143a0868c6bb6
Andy T <andytruong@users.noreply.github.com>
2017-09-08 15:28:59 +0700

    Update service-ip.php

--- scripts/service-ip.php
+++ scripts/service-ip.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace go1\monolith\scripts;
+
 /**
  * Get service IP
  *
@@ -10,13 +13,16 @@
  * - Configure aws http://docs.aws.amazon.com/cli/latest/reference/configure/
  */
 
+use RuntimeException;
 
 if (empty($argv[1])) {
     throw new RuntimeException("Missing cluster name");
 }
+
 if (empty($argv[2])) {
     throw new RuntimeException("Missing service name");
 }
+
 $cluster = $argv[1];
 $service = $argv[2];
 $service = "ecscompose-service-$service";
@@ -34,12 +40,14 @@ $instanceId = explode("/", $task)[1];
 if (empty($instanceId)) {
     throw new RuntimeException("Failed to find container instance id.");
 }
+
 $instance = shell_exec("aws ecs describe-container-instances --cluster=$cluster --container-instances=$instanceId");
 $instance = json_decode($instance, true);
 $instanceId = $instance['containerInstances'][0]['ec2InstanceId'];
 if (empty($instanceId)) {
     throw new RuntimeException("Failed to find instance id.");
 }
+
 $instance = shell_exec("aws ec2 describe-instances --instance-ids=$instanceId");
 $instance = json_decode($instance, true);
 $ip = $instance['Reservations'][0]['Instances'][0]['PublicDnsName'];
