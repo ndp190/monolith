ba9ce32ef0d231afd67152e18fe41c3f44b4b1c6
Andy Truong <thehongtt@gmail.com>
2017-11-17 11:24:07 +0700

    scripts/launch > Fix hard code in .data/resources/docker/fix-*.php

--- scripts/launch.php
+++ scripts/launch.php
@@ -11,6 +11,16 @@ require_once $pwd . '/php/vendor/autoload.php';
 $client = new Client;
 $custom = $pwd . '/build.json';
 $custom = is_file($custom) ? json_decode(file_get_contents($custom), true) : [];
+$domain = isset($custom['features']['domain']) ? $custom['features']['domain'] : null;
+
+# Fix hard code in .data/resources/docker/fix-*.php
+$fix[] = $pwd . '/.data/resources/docker/fix-apiom-ui.php';
+$fix[] = $pwd . '/.data/resources/docker/fix-website.php';
+foreach ($fix as $file) {
+    $source = file_get_contents($file);
+    $source = str_replace('http://localhost/GO1', 'http://' . $domain . '/GO1', $source);
+    file_put_contents($file, $source);
+}
 
 # Start docker compose
 require __DIR__ . '/start.php';
