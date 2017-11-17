3daef2be27ca49f41f6c024d886bcac8156bba06
Andy Truong <thehongtt@gmail.com>
2017-11-17 12:17:16 +0700

    scripts/build-git-pull > Only need single branch.

--- scripts/build-git-pull.php
+++ scripts/build-git-pull.php
@@ -4,6 +4,7 @@ namespace go1\monolith\scripts;
 
 return function ($pwd, $projects, $custom) {
     $defaultBranch = 'master';
+    $single = !empty($custom);
 
     foreach ($projects as $lang => $services) {
         foreach ($services as $name => $path) {
@@ -11,8 +12,14 @@ return function ($pwd, $projects, $custom) {
             $target = ('golang' === $lang) ? "$pwd/$lang/src/vendor/go1/$name" : "$pwd/$lang/$name";
 
             if (!is_dir($target)) {
-                echo "git clone -q --branch=$branch $path $target\n";
-                passthru("git clone -q --branch=$branch $path $target");
+                if ($single) {
+                    echo "git clone -q --single-branch --branch=$branch $path $target\n";
+                    passthru("git clone -q --single-branch --branch=$branch $path $target");
+                }
+                else {
+                    echo "git clone -q --branch=$branch $path $target\n";
+                    passthru("git clone -q --branch=$branch $path $target");
+                }
             }
         }
     }
