b4b6e89cfd55335d367ed39c304d49416f97728c
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-12-01 00:08:17 +0700

    Improve performance of scormengine on mac os

--- scripts/stop.php
+++ scripts/stop.php
@@ -4,8 +4,10 @@ namespace go1\monolith\scripts;
 
 if (PHP_OS === 'Darwin') {
     passthru('docker-compose stop');
+    passthru('docker-compose -f docker-compose-scorm.yml stop');
     passthru('docker-sync stop');
 }
 elseif (PHP_OS === 'Linux' || PHP_OS === 'Windows') {
     passthru('docker-compose stop');
+    passthru('docker-compose -f docker-compose-scorm.yml stop');
 }
