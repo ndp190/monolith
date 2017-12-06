5d530905771978dd60309e85e19fb53b1030f753
Phuong Huynh <htphuong290@gmail.com>
2017-12-06 12:28:33 +0700

    Add ENV_HOSTNAME env

--- scripts/start.php
+++ scripts/start.php
@@ -2,8 +2,8 @@
 
 namespace go1\monolith\scripts;
 
-$pwd = dirname(__DIR__);
-$custom = is_file($pwd . '/build.json');
+$pwd       = dirname(__DIR__);
+$hasCustom = is_file($pwd . '/build.json');
 
 @mkdir("$pwd/.data");
 @mkdir("$pwd/.data/nginx");
@@ -11,14 +11,19 @@ $custom = is_file($pwd . '/build.json');
 @unlink("$pwd/.data/nginx/sites-available/default.conf");
 @copy("$pwd/.data/nginx/app.conf", "$pwd/.data/nginx/sites-available/default.conf");
 
-$ip = require 'ip.php';
-$ip = str_replace("\n", '', $ip);
-$custom = $custom ? ' -d' : '';
+$ip        = require 'ip.php';
+$ip        = str_replace("\n", '', $ip);
+$extraArgs = $hasCustom ? ' -d' : '';
+$domain    = 'localhost';
+if ($hasCustom) {
+    $custom = json_decode(file_get_contents($pwd . '/build.json'), true);
+    $domain = !empty($custom['features']['domain']) ? $custom['features']['domain'] : $domain;
+}
 
 if (PHP_OS === 'Darwin') {
     passthru('docker-sync start');
-    passthru("MONOLITH_HOST_IP='{$ip}' docker-compose -f docker-compose.yml -f docker-compose-dev.yml up --force-recreate {$custom}");
+    passthru("MONOLITH_HOST_IP='{$ip}' ENV_HOSTNAME={$domain} docker-compose -f docker-compose.yml -f docker-compose-dev.yml up --force-recreate {$extraArgs}");
 }
 elseif (PHP_OS === 'Linux' || PHP_OS === 'Windows') {
-    passthru("MONOLITH_HOST_IP='{$ip}' docker-compose -f {$pwd}/docker-compose.yml up --force-recreate {$custom}");
+    passthru("MONOLITH_HOST_IP='{$ip}' ENV_HOSTNAME={$domain} docker-compose -f {$pwd}/docker-compose.yml up --force-recreate {$extraArgs}");
 }
