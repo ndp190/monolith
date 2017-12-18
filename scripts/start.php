025b6c901af44c57c004537b05c361e619cf8135
Andy Truong <thehongtt@gmail.com>
2017-12-18 13:15:05 +0700

    Fix call build-cron.php

--- scripts/start.php
+++ scripts/start.php
@@ -20,5 +20,5 @@ if ($hasCustom) {
 $cmd = implode(' ', $argv);
 $scorm = false !== strpos($cmd, '--with-scorm') ? "-f {$pwd}/docker-compose-scorm.yml" : '';
 
-passthru("php {$pwd}/infrastructure/cron/build-cron.php");
+passthru("MONOLITH=1 php {$pwd}/infrastructure/cron/build-cron.php");
 passthru("MONOLITH_HOST_IP='{$ip}' ENV_HOSTNAME={$domain} docker-compose -f {$pwd}/docker-compose.yml {$scorm} up --force-recreate {$extraArgs}");
